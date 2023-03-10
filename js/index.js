6f35eccfac28910a51817bae2fcf8189848816d6
joelletiangco <86069672+joelletiangco@users.noreply.github.com>
2023-03-10 00:46:40 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 const playText = document.getElementById('play-text');
 const playButton = document.getElementById('play-button');
+const homepageCard = document.getElementById('homepage-card');
 
 playText.addEventListener('click', () => {
   window.location.href = 'decks.html';
 });
 
 playButton.addEventListener('click', () => {
+  window.location.href = 'decks.html';
+});
+
+homepageCard.addEventListener('click', () => {
   window.location.href = 'decks.html';
 });
